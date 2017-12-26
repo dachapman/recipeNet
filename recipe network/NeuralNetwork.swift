8b07f990741bbe90b115425349be5c04db7b56a7
Dee Chapman <dachapman@dees-imac.local>
2017-12-26 11:37:56 -0600

    adds inference function

--- recipe network/NeuralNetwork.swift
+++ recipe network/NeuralNetwork.swift
@@ -15,6 +15,7 @@ enum NetworkActivation {
 
 class NeuralNetwork: NSObject {
     
+    // "weights" holds all the weight values between layers.
     var weights : [[[Double]]] = []
     
     // default value is sigmoid activation function, but can also set it to relu
@@ -26,7 +27,7 @@ class NeuralNetwork: NSObject {
     
     func initRandomWeights(layerWidth: Array<Int>) {
         for layer in 0..<(layerWidth.endIndex - 1) {
-            var nextLayerWidth = layerWidth[layer + 1]
+            let nextLayerWidth = layerWidth[layer + 1]
             var layerWeights : [[Double]] = []
             // create random weights for each layer
             for _ in 0..<layerWidth[layer] {
@@ -43,7 +44,7 @@ class NeuralNetwork: NSObject {
         }
     }
     
-    func train() {
+    func train(input: Array<Double>, desiredOutput: Array<Double>) {
         
     }
     
@@ -51,8 +52,28 @@ class NeuralNetwork: NSObject {
         
     }
     
-    func inference() {
-        
+    // run input through network and return output, which will be a 2d array of activations (0.0-1.0) of each neuron
+    func inference(input: Array<Double>) -> Array<Array<Double>> {
+        var neuronActivation: [[Double]] = []
+        // first layer is always the input we are given
+        neuronActivation.append(input)
+        for layer in 0..<weights.count {
+            let numberNeuronsInNextLayerWidth = weights[layer][0].count
+            // initialize activations to 0.0 for next layer neurons
+            neuronActivation.append(Array(repeatElement(0.0, count: numberNeuronsInNextLayerWidth)))
+            // calculate the new activations
+            for i in 0..<weights[layer].count {
+                // iterate over next layer
+                for j in 0..<weights[layer][i].count {
+                    // accumulate the activations from all inbound previous layer activations * weights
+                    neuronActivation[layer+1][j] += (weights[layer][i][j] * neuronActivation[layer][i])
+                }
+            }
+            // use activation function as our nonlinearity to force neuronActivations to be between 0.0 - 1.0
+            // on our layer + 1 (type of activation is defined by NetworkActivation enum (sigmoid or relu)
+            neuronActivation[layer + 1] = neuronActivation[layer + 1].map({activation(number: $0)})
+        }
+        return neuronActivation
     }
     
     func activation(number: Double) -> Double {
